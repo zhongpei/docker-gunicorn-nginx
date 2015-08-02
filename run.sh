aaf56674ff779bce45771b4f81bc46966116e190
vagrant <vagrant@vagrant.vm>
2015-08-02 12:00:56 +0000

    fix

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --name django-webserver -p 8080:80 --link m1-mysql:mysql --link m1-mongo:mongodb   -P -t -i -v /vagrant/webprobe:/opt/app zhongpei/django-gunicorn-nginx
+docker run --name django-webserver -p 80:80 --link m1-mysql:mysql --link m1-mongo:mongodb   -P -t -i -v /vagrant/webprobe:/opt/app -v /vagrant/static:/opt/app/static zhongpei/django-gunicorn-nginx
